3799dd0e4ee57aa0957d7a3a1116b36b98fb81a9
Pratiksha <pratikshajadkar@gmail.com>
2023-03-07 05:57:38 +0530

    java files added

---
+++ dir1/add.java
@@ -0,0 +1,9 @@
+public class add
+{
+    public static void main (String args[])
+    {
+        int a = 10 , b=15,add ;
+       add= a - b;
+        system.out.println("add : " +add);
+    }
+}
---
+++ dir2/Diff.java
@@ -0,0 +1,9 @@
+public class diff
+{
+    public static void main (String args[])
+    {
+        int a = 10 , b=15,diff ;
+         diff= a - b;
+        system.out.println("diff : " + diff);
+    }
+}
