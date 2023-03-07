b1e03cb4e3b037b7675d61fd4cf403fab128e445
Pratiksha <pratikshajadkar@gmail.com>
2023-03-07 06:59:00 +0530

    added division part in diff.java file

--- dir1/diff.java
+++ dir1/diff.java
@@ -2,10 +2,12 @@ public class diff
 {
     public static void main (String args[])
     {
-        int a = 10 , b=15,diff,mult ;
+        int a = 10 , b=15,diff,mult,div;
          diff= a - b;
         System.out.println("diff : " + diff);
         mult = a*b;
         System.out.println("mult"+mult);
+        div = a/b;
+        System.out.println("div"+div);
     }
 }
